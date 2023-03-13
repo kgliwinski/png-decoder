1db242fcc1483932f64680fbdda04f2f6c98609a
kgliwinski <259282@student.pwr.edu.pl>
2023-03-13 22:45:59 +0100

    Added get_header and get_trailer methods

--- application/main.py
+++ application/main.py
@@ -1,7 +1,9 @@
 import png_class
 import logging as log
 
-log.basicConfig(format='%(asctime)s %(message)s', level=log.INFO)
+log.basicConfig(format='%(asctime)s %(message)s', level=log.DEBUG)
 png = png_class.Png('cat.png')
 
 png.get_signature()
+png.get_header()
+png.get_trailer()
--- application/png_class.py
+++ application/png_class.py
@@ -38,11 +38,39 @@ class Png:
             self.signature.append(byte)
             if byte == b'\n':
                 break
-        log.debug("Read signature:\n %s", self.signature)
+        log.debug("Read signature:\n %s\n", self.signature)
         return len(self.signature)
 
     def get_header(self):
-        pass
+        while (byte := self.file_png.read(1)):
+            self.header["length"].append(byte)
+            if byte == b'\n':
+                break
+        while (byte := self.file_png.read(1)):
+            self.header["type"].append(byte)
+            if byte == b'\n':
+                break
+        while (byte := self.file_png.read(1)):
+            self.header["data"].append(byte)
+            if byte == b'\n':
+                break
+        while (byte := self.file_png.read(1)):
+            self.header["crc32"].append(byte)
+            if byte == b'\n':
+                break
+        log.debug("Read header:\n %s\n", self.header)
 
     def get_trailer(self):
-        pass
+        while (byte := self.file_png.read(1)):
+            self.trailer["length"].append(byte)
+            if byte == b'\n':
+                break
+        while (byte := self.file_png.read(1)):
+            self.trailer["type"].append(byte)
+            if byte == b'\n':
+                break
+        while (byte := self.file_png.read(1)):
+            self.trailer["crc32"].append(byte)
+            if byte == b'\n':
+                break
+        log.debug("Read trailer:\n %s\n", self.trailer)
