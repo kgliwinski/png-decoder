56d6e896c56f659c9b68542073e7bd26f82c9962
kgliwinski <259282@student.pwr.edu.pl>
2023-05-08 03:15:37 +0200

    No work

--- application/encryption.py
+++ application/encryption.py
@@ -2,12 +2,16 @@ import numpy as np
 import sympy
 import random
 import chunk_class as chunk
+import logging as log
 # https://github.com/Vipul97/des
 # https://www.techtarget.com/searchsecurity/definition/Electronic-Code-Book
 # https://en.wikipedia.org/wiki/RSA_(cryptosystem)
  
 
 class rsa2048:
+
+    ENCRYPT_BLOCK_SIZE = 256
+
     def __init__(self, chunks_to_encrypt : list, public_key : tuple = None, private_key : tuple = None): 
         """ 
         # Chunk RSA2048 encryption
@@ -45,7 +49,7 @@ class rsa2048:
         """ Returns private key"""
         return self.private_key
     
-    def encrypt_chunk(self, chunk : chunk.Chunk) -> chunk.Chunk:
+    def encrypt_chunk(self, chunk_to_encrypt : chunk.Chunk) -> chunk.Chunk:
         """ 
         # Encrypt chunk
         ## Args:
@@ -53,10 +57,21 @@ class rsa2048:
         ## Returns:
             - bytes: encrypted chunk
         """        
-        chunk_int = int.from_bytes(chunk.get_chunk(), byteorder='big')
-        encrypted_chunk_int : int = pow(chunk_int, self.public_key[1], self.public_key[0])
-        chunk.replace_chunk_data(encrypted_chunk_int.to_bytes(chunk.get_chunk_size(), byteorder='big'))
-        return chunk
+        chunk_data = chunk_to_encrypt.get_chunk()
+        # divide chunk into self.ENCRYPT_BLOCK_SIZE byte chunks
+        chunk_data_blocks = [chunk_data[i:i+self.ENCRYPT_BLOCK_SIZE] for i in range(0, len(chunk_data), self.ENCRYPT_BLOCK_SIZE)]
+        for i in chunk_data_blocks:
+            print(len(i))
+        encrypted_chunk_data_blocks = b''
+        for chunk_block in chunk_data_blocks:
+            chunk_int = int.from_bytes(chunk_block, byteorder='big')
+            encrypted_chunk_int : int = pow(chunk_int, self.public_key[1], self.public_key[0])
+            encrypted_chunk_data_blocks += encrypted_chunk_int.to_bytes(self.ENCRYPT_BLOCK_SIZE, byteorder='big')
+            if len(chunk_block) < self.ENCRYPT_BLOCK_SIZE:
+                encrypted_chunk_data_blocks = encrypted_chunk_data_blocks[:-self.ENCRYPT_BLOCK_SIZE + len(chunk_block)]
+            log.info(f'Encrypted chunk: {encrypted_chunk_int}')
+        chunk_to_encrypt.replace_chunk_data(encrypted_chunk_data_blocks)
+        return chunk_to_encrypt
 
     def encrypt_all_chunks(self) -> list:
         """ 
@@ -79,7 +94,7 @@ class rsa2048:
         """        
         chunk_int = int.from_bytes(chunk.get_chunk(), byteorder='big')
         decrypted_chunk_int = pow(chunk_int, self.private_key[1], self.private_key[0])
-        chunk.replace_chunk_data(decrypted_chunk_int.to_bytes(256, byteorder='big'))
+        chunk.replace_chunk_data(decrypted_chunk_int.to_bytes(self.ENCRYPT_BLOCK_SIZE, byteorder='big'))
         return chunk
     
     def decrypt_all_chunks(self) -> list:
--- application/test.py
+++ application/test.py
@@ -1,10 +1,13 @@
 import png_class as png
 import cv2 as cv
+import logging as log
 
-cat = png.Png('pictures/cat.png')
+log.basicConfig(
+    format='%(levelname)s {%(pathname)s:%(lineno)d} %(asctime)s %(message)s', level=log.INFO)
+cat = png.Png('pictures/spiderman.png')
 print(cat.get_chunk_types())
 
-encrypted = png.EncryptedPng('pictures/cat.png')
+encrypted = png.EncryptedPng('pictures/spiderman.png')
 encrypted.build_png_from_chunks('.tmp/encrypted.png')
 print(encrypted.get_chunk_types())
 img = cv.imread('.tmp/encrypted.png')
